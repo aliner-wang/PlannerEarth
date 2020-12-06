f5f4c810bea3ce8bf474a1cbb19f8139427a7603
aliner-wang <alina.wang@berkeley.edu>
2020-12-06 01:41:12 -0800

    dorta backend?

---
+++ backend/index.js
@@ -0,0 +1,115 @@
+/* initializes express app => this is equivalent to getting the backend up and running */
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var router = express.Router();
+
+
+// creates mongo db connection
+const mongoose = require('mongoose')
+const url = 'mongodb://127.0.0.1:27017/database-tutorial'
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const db = mongoose.connection
+db.once('open', _ => {
+    console.log('Database connected:', url)
+
+})
+
+// if connection errors return error report. 
+db.on('error', err => {
+    console.error('connection error:', err)
+})
+
+
+const Schema = mongoose.Schema
+
+const todoSchema = new Schema({
+    content: String,
+})
+
+const TODO = mongoose.model("TODO", todoSchema)
+
+// to recieve inputs
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+
+// get returns all the todos
+// post creates a new todo. 
+router.route('/todos')
+    .get((req, res) => {
+        // in charge of returning all todos
+        TODO.find().then((todos) => {
+            res.json({ message: "Return all todos.", todos: todos });
+        })
+    })
+
+
+.post((req, res) => {
+    const todo = new TODO({
+        content: req.body.content
+
+    })
+    todo.save((error, document) => {
+        res.json({
+            message: ("Create a new todo: " + "1" + "-" + req.body.contect),
+            todo_id: "1",
+            content: req.body.content,
+        });
+    })
+})
+
+
+
+//todo gets a particular todo task given a todo_id
+router.route('/todos/:todo_id')
+
+.get((req, res) => {
+    TODO.findById(req.params.todo_id, (err, todo) => {
+        if (err) {
+            res.json({ message: 'Error when getting the content of the todo' });
+        }
+        res.json({ message: 'Get the content from a todo.'.todo });
+    })
+
+})
+
+.put((req, res) => {
+    TODO.findByIdAndUpdate(req.params.todo_id, { content: req.body.content }, (err, todo) => {
+        if (err) {
+            res.json({ message: "Error when updating todo." });
+        }
+        res.json({ message: 'Update the todo: ' + req.params.todo_id, old: todo, new: { _id: req.params.todo_id, content: req.body, content } });
+
+    });
+})
+
+
+
+router.route('/todos/:todo_id')
+
+.delete((req, res) => {
+    TODO.findByIdAndDelete(req.params.todo_id, (err, todo) => {
+        if (err) {
+            res.json({ message: "Failed to delete todo." });
+        }
+
+        res.json({ message: "Delete the todo: " + todo._id, todo: { _id: todo._id, cotent: todo.content } });
+
+    });
+})
+
+
+
+//updates a task 
+
+
+// assign port for express to listen to 
+app.listen(3000, () => console.log("Server Up and running"));
--- src/App.js
+++ src/App.js
@@ -1,79 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Form from './components/ToDoList/form.js';
 import TodoList from './components/ToDoList/todolist.js'
 
-
-//React DF
 function App() {
-    //Input text = what the person types into the bar for their task
-    const [inputText, setInputText] = useState("");
-    //List of all todos
-    const [todos, setTodos] = useState([]);
-    //Filter status - if they want to see all, completed, uncompleted, important tasks
-    const [status, setStatus] = useState("all");
-    //List of filtered todos - needed another list bc user might click all again and want original list saved as well
-    const [filteredTodos, setFilteredTodos] = useState([]);
+  //Input text = what the person types into the bar for their task
+  const [inputText, setInputText] = useState("");
+  //List of all todos
+  const [todos, setTodos] = useState([]);
+  //Filter status - if they want to see all, completed, uncompleted, important tasks
+  const [status, setStatus] = useState("all");
+  //List of filtered todos - needed another list bc user might click all again and want original list saved as well
+  const [filteredTodos, setFilteredTodos] = useState([]);
 
-    useEffect(() => {
-        filterHandler();
-    }, [todos, status]);
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
 
-    //Filter out what the user wants to see based on category
-    const filterHandler = () => {
-        switch (status) {
-            case ("completed"):
-                setFilteredTodos(todos.filter(todo => todo.completed === true))
-                break;
-            case ("uncompleted"):
-                setFilteredTodos(todos.filter(todo => todo.completed === false))
-                break;
-            default:
-                setFilteredTodos(todos)
-                break;
-        }
-    };
+  //Filter out what the user wants to see based on category
+  const filterHandler = () => {
+    switch(status) {
+      case("completed"):
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case("uncompleted"):
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break;
+      default:
+        setFilteredTodos(todos)
+        break;
+    }
+  };
 
-    return ( <
-        div className = "App" >
-        <
-        Router >
-        <
-        Navbar / >
-        <
-        Switch >
-        <
-        Route path = '/' / >
-        <
-        /Switch> <
-        /Router>
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' />
+        </Switch>
+      </Router>
+  
+    <header>
+      <h1> My To-Do List</h1>
+    </header>
+    <Form
+      todos={todos} setTodos={setTodos} setInputText = {setInputText}
+      inputText={inputText} status={status} setStatus={setStatus}/>
 
-        <
-        header >
-        <
-        h1 > My To - Do List < /h1> <
-        /header> <
-        Form todos = { todos }
-        setTodos = { setTodos }
-        setInputText = { setInputText }
-        inputText = { inputText }
-        status = { status }
-        setStatus = { setStatus }
-        />
-
-        <
-        TodoList setTodos = { setTodos }
-        todos = { todos }
-        filteredTodos = { filteredTodos }
-        /> <
-        /div>
-    );
+      <TodoList setTodos={setTodos} todos={todos} filteredTodos = {filteredTodos}/>
+    </div>
+  );
 }
 
-
-
-
-// Express connecting to Data Base and grabbing the goods. 
 export default App;
