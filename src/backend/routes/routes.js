7a4c0c8bf2fe9f200ddbb585b0fad6160f72983a
Jerrick <jerrickzhu@berkeley.edu>
2020-12-08 19:46:33 -0800

    added texting backend thingy

--- src/backend/routes/routes.js
+++ src/backend/routes/routes.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const todoTemplateCopy = require('../models/todoModels')
+var text = "";
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 router.route('/todos')
   // ...
@@ -17,6 +25,17 @@ router.route('/todos')
         content: req.body.content,
       }); 
     })
+    text = req.body.content;
+    client.messages.create({
+      body: text,
+      from: '+12486005215',
+      to: '+18582006636'
+    })
+    .then(message => console.log(message))
+    .catch(e => console.log(e))
   })
 
 module.exports = router
+
+
+
